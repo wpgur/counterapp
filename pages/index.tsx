2b74ad7d341bea94980b92148a735fd07300d34f
wpgur <wpgur0510@naver.com>
2023-04-05 21:33:01 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,96 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import {
+  ConnectWallet,
+  useAddress,
+  useContract,
+  Web3Button,
+} from '@thirdweb-dev/react';
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  //내 주소 불러오기
+  const myAddress = useAddress();
+  //contract 연결
+  const contractAddress = '0x3eAA24cf8927A9875751b96dbF729C44d9208da6';
+  const { contract, isLoading } = useContract(contractAddress);
+
+  //react hook이라는 기술
+  //setCounter함수가 쓰이면 counter가 바뀜
+  const [counter, setCounter] = useState<string | undefined>(undefined);
+
+  //counter 가져오기 함수
+  async function getCounter() {
+    if (!contract) return;
+
+    const counter = await contract.call('getCounter');
+    setCounter(counter.toString());
+  }
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
         <div className={styles.connect}>
           <ConnectWallet />
         </div>
+        <p>My Address : {myAddress} </p>
+        <h1>Counter Dapp</h1>
+        <h3>{counter}</h3>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
+        <Web3Button
+          contractAddress={contractAddress}
+          action={() => getCounter()}
+        >
+          Refresh Counter
+        </Web3Button>
+        <br />
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        <Web3Button
+          contractAddress={contractAddress}
+          action={() => contract?.call('incrementCounter')}
+        >
+          +
+        </Web3Button>
       </main>
     </div>
   );
 };
 
 export default Home;
+
+// import { ConnectWallet, useContract, Web3Button } from '@thirdweb-dev/react'
+// import type { NextPage } from 'next'
+// import { useState } from 'react'
+// import styles from '../styles/Home.module.css'
+// const Home: NextPage = () => {
+//  const contractAddress = '0x8425CB106b5ef9B2475FE3428d6daaA645F01670'
+//  const { contract } = useContract(contractAddress)
+//  const [counter, setCounter] = useState<string | undefined>(undefined)
+//  async function getCounter() {
+//   if (!contract) return
+//   const counter = await contract.call('getCounter')
+//   setCounter(counter.toString())
+//  }
+//  return (
+//   <div className={styles.container}>
+//    <main className={styles.main}>
+//     <h1>Counter Dapp</h1>
+//     <h3>{counter}</h3>
+//     <Web3Button
+//      contractAddress={contractAddress}
+//      action={() => getCounter()}
+//     >
+//      Refresh Counter
+//     </Web3Button>
+//     <br />
+//     <Web3Button
+//      contractAddress={contractAddress}
+//      action={(contract) => contract.call('incrementCounter')}
+//     >
+//      Increment Counter
+//     </Web3Button>
+//    </main>
+//   </div>
+//  )
+// }
+// export default Home
